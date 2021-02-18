6c2f700f903b9bc1eccbc08ff0ff4a0ce3f097a8
Liam Gulliver <128083+lgulliver@users.noreply.github.com>
2021-02-18 20:32:47 +0000

    added caritem and mongodb

---
+++ CarsUnlimited.Shared/Models/CarItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CarsUnlimited.Shared.Models
+{
+    public class CarItem
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string CarPicture { get; set; }
+        public string CarManufacturer { get; set; }
+        public string CarModel { get; set; }
+        public string CarInfo { get; set; }
+        public double CarPrice { get; set; }
+        public int CarsInStock { get; set; }
+    }
+}
