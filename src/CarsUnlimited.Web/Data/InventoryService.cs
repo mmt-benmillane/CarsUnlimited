c651e9a1453c557a9578bb53b2f87a24c317adfc
Liam Gulliver <liam.gulliver@mmtdigital.co.uk>
2021-03-09 23:26:38 +0000

    inventory in webui

---
+++ src/CarsUnlimited.Web/Data/InventoryService.cs
@@ -0,0 +1,59 @@
+﻿using CarsUnlimited.InventoryShared.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Net.Http.Json;
+using Microsoft.Extensions.Logging;
+
+namespace CarsUnlimited.Web.Data
+{
+    public class InventoryService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<InventoryService> _logger;
+
+        public InventoryService(IConfiguration configuration, ILogger<InventoryService> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public async Task<List<CarItem>> GetInventoryAsync()
+        {
+            _logger.LogInformation("Begin GetInventoryAsync");
+
+            var apiBaseUrl = $"{_configuration.GetSection("ApiGatewayUrl").Value}";
+            var apiKey = _configuration.GetSection("ApiKey").Value;
+
+            using(HttpClient client = new())
+            {
+                client.BaseAddress = new Uri(apiBaseUrl);
+                client.DefaultRequestHeaders.Add("X-CarsUnlimited-ApiKey", apiKey);
+
+                try
+                {
+                    var inventoryTask = await client.GetFromJsonAsync<List<CarItem>>("api/inventory/");
+
+                    if(inventoryTask != null)
+                    {
+                        List<CarItem> carItems = inventoryTask;
+                        return carItems;
+                    } 
+                    else
+                    {
+                        _logger.LogError($"Error retrieving inventory. No content");
+                        return new List<CarItem>();
+                    }
+                } catch (HttpRequestException ex)
+                {
+                    _logger.LogError($"Error retrieving inventory. Error: {ex.Message}");
+                    return new List<CarItem>();
+                }
+            }
+        }
+    }
+}
--- src/CarsUnlimited.Web/Startup.cs
+++ src/CarsUnlimited.Web/Startup.cs
@@ -1,3 +1,4 @@
+using CarsUnlimited.Shared.Configuration;
 using CarsUnlimited.Web.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
@@ -10,11 +11,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace CarsUnlimited.Web
 {
     public class Startup
     {
+
+        private readonly string _serviceName = "CarsUnlimited.Web";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,9 +32,58 @@ namespace CarsUnlimited.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            TracingConfiguration tracingConfig = Configuration.GetSection("TracingConfiguration").Get<TracingConfiguration>();
+
+            TraceExporterOptions exporter = (TraceExporterOptions)tracingConfig.Exporter;
+
+            switch (exporter)
+            {
+                case TraceExporterOptions.JAEGER:
+                    services.AddOpenTelemetryTracing((builder) => builder
+                        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(_serviceName))
+                        .AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation()
+                        .AddJaegerExporter(jaegerOptions =>
+                        {
+                            jaegerOptions.AgentHost = tracingConfig.JaegerEndpoint.Host;
+                            jaegerOptions.AgentPort = tracingConfig.JaegerEndpoint.Port;
+                        }));
+                    break;
+                case TraceExporterOptions.ZIPKIN:
+                    services.AddOpenTelemetryTracing((builder) => builder
+                        .AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation()
+                        .AddZipkinExporter(zipkinOptions =>
+                        {
+                            zipkinOptions.Endpoint = new Uri(tracingConfig.ZipkinEndpoint);
+                        }));
+                    break;
+                case TraceExporterOptions.OPENTELEMETRY_PROTOCOL:
+                    // Adding the OtlpExporter creates a GrpcChannel.
+                    // This switch must be set before creating a GrpcChannel/HttpClient when calling an insecure gRPC service.
+                    // See: https://docs.microsoft.com/aspnet/core/grpc/troubleshoot#call-insecure-grpc-services-with-net-core-client
+                    AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+                    services.AddOpenTelemetryTracing((builder) => builder
+                        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(_serviceName))
+                        .AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation()
+                        .AddOtlpExporter(otlpOptions =>
+                        {
+                            otlpOptions.Endpoint = new Uri(tracingConfig.OltpEndpoint);
+                        }));
+                    break;
+                default:
+                    services.AddOpenTelemetryTracing((builder) => builder
+                        .AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation()
+                        .AddConsoleExporter());
+                    break;
+            }
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<InventoryService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
