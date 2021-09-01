d5e76c751dd4b4c71140c0a2a4d914c807169062
Ben Millane <ben.millane@mmtdigital.co.uk>
2021-09-01 16:59:35 +0100

    Added CartApi Unit Test Project

---
+++ src/CarsUnlimited.CartAPI.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarsUnlimited.CartAPI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+
+}
