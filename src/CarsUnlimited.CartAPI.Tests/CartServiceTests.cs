c0458169c033af4bc3b3a0f3b1c663393b53797c
Ben Millane <ben.millane@mmtdigital.co.uk>
2021-09-02 10:40:45 +0100

    added in first full unit test for CartAPI

--- src/CarsUnlimited.CartAPI.Tests/CartServiceTests.cs
+++ src/CarsUnlimited.CartAPI.Tests/CartServiceTests.cs
@@ -1,6 +1,13 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using CarsUnlimited.CartAPI;
+using CarsUnlimited.CartAPI.Services;
 using CarsUnlimited.CartShared.Entities;
+using StackExchange.Redis.Extensions.Core.Abstractions;
+using Moq;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using System;
+using StackExchange.Redis;
+using System.Threading.Tasks;
 
 namespace CarsUnlimited.CartAPI.Tests
 {
@@ -8,9 +15,19 @@ namespace CarsUnlimited.CartAPI.Tests
     public class CartServiceTests
     {
         [TestMethod]
-        public void GivenCartItem_WhenAddedToCart_ThenItemIsAddedToCart()
+        public async Task GivenCartItem_WhenAddedToCart_ThenItemIsAddedToCart()
         {
-            Assert.IsTrue(true);
+            var mockIRedisCacheClient = new Mock<IRedisCacheClient>();
+            var mockILogger = new Mock<ILogger<CartService>>();
+            var mockIConfiguration = new Mock<IConfiguration>();
+            var cartItem = new CartItem();
+
+            mockIRedisCacheClient.Setup(x => x.GetDbFromConfiguration().AddAsync<CartItem>(It.IsAny<string>(), It.IsAny<CartItem>(), 
+                                            It.IsAny<DateTimeOffset>(), It.IsAny<When>(), It.IsAny<CommandFlags>())).ReturnsAsync(true);
+
+            CartService service = new CartService(mockIRedisCacheClient.Object, mockILogger.Object, mockIConfiguration.Object);
+            var result = await service.AddToCart(cartItem);
+            Assert.AreEqual(true, result);
         }
     }
 }
