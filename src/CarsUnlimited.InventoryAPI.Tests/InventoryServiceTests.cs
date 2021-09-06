4a1f56cc0ef75b096ae3f4c9088e581e2aa6074d
Ben Millane <ben.millane@mmtdigital.co.uk>
2021-09-06 14:47:36 +0100

    added in two unit tests for inventory, checking the get for a specific car id and also updating a car, ensure repo ReplaceOne method is called correctly

--- src/CarsUnlimited.InventoryAPI.Tests/InventoryServiceTests.cs
+++ src/CarsUnlimited.InventoryAPI.Tests/InventoryServiceTests.cs
@@ -24,6 +24,7 @@ namespace CarsUnlimited.InventoryAPI.Tests
                 new CarItem{ Id="two", CarInfo="two car info", CarManufacturer= "TwoManufacturer", CarModel="TwoCarModel", CarPicture="TwoCarPicture", CarPrice=200, CarsInStock=2  } 
                 }.AsQueryable<CarItem>() );
 
+            _carItemRepository.Setup( x => x.FindById("one")).Returns( new CarItem { Id="one", CarInfo="one car info", CarManufacturer= "OneManufacturer", CarModel="OneCarModel", CarPicture="OneCarPicture", CarPrice=100, CarsInStock=1 } );
         }
 
         [TestMethod]
@@ -35,5 +36,28 @@ namespace CarsUnlimited.InventoryAPI.Tests
             Assert.AreEqual("TwoManufacturer", result[1].CarManufacturer );
             Assert.AreEqual(2, result.Count);
         }
+
+        [TestMethod]
+        public void GivenAtLeastOneCarInInventory_WhenSpecificCarIsRequested_ThenSpecificCarIsReturned()
+        {
+            InventoryService inventoryService = new InventoryService(_carItemRepository.Object);
+            var result = inventoryService.Get("one");
+            Assert.AreEqual(result.Id, "one");
+            Assert.AreEqual(result.CarInfo, "one car info");
+            Assert.AreEqual(result.CarManufacturer, "OneManufacturer");
+            Assert.AreEqual(result.CarModel, "OneCarModel");
+            Assert.AreEqual(result.CarPicture, "OneCarPicture");
+            Assert.AreEqual(result.CarPrice, 100);
+            Assert.AreEqual(result.CarsInStock, 1);
+        }
+
+        [TestMethod]
+        public void GivenACar_WhenItIsUpdated_ThenUpdateIsApplied()
+        {
+            InventoryService inventoryService = new InventoryService(_carItemRepository.Object);
+            inventoryService.Update( new CarItem { Id="one" } );
+            _carItemRepository.Verify( x => x.ReplaceOne(It.IsAny<CarItem>()), Times.Once );
+        }
+
     }
 }
