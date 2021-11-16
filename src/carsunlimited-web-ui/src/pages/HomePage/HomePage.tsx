8c80680edc5eb49ef6d2017c9c70a827f459f934
Liam Gulliver <128083+lgulliver@users.noreply.github.com>
2021-11-16 17:03:06 +0000

    start of pages

---
+++ src/carsunlimited-web-ui/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './HomePage.module.css';
+import Header from './../../components/Header/Header';
+
+const HomePage = () => (
+  <div className={styles.HomePage} data-testid="HomePage">
+    HomePage Component
+  </div>
+);
+
+export default HomePage;
