9b4900bb4214665c1a37dfb469c9b0218baf68e5
Liam Gulliver <128083+lgulliver@users.noreply.github.com>
2021-11-16 15:06:25 +0000

    revert react app to basic

--- src/carsunlimited-web-ui/src/index.js
+++ src/carsunlimited-web-ui/src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
-import { mergeStyles } from '@fluentui/react';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Inject some global styles
-mergeStyles({
-  ':global(body,html,#root)': {
-    margin: 0,
-    padding: 0,
-    height: '100vh',
-  },
-});
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
