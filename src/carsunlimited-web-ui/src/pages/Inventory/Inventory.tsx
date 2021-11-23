1a46a635b3de7f371bddc7440a63ed3f1ca2cb03
Liam Gulliver <128083+lgulliver@users.noreply.github.com>
2021-11-23 15:04:23 +0000

    add layout to inventory

--- src/carsunlimited-web-ui/src/pages/Inventory/Inventory.tsx
+++ src/carsunlimited-web-ui/src/pages/Inventory/Inventory.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import styles from './Inventory.module.css';
+import React from "react";
+
+import Layout from "../../layouts/MainLayout";
+
+import styles from "./Inventory.module.css";
 
 const Inventory = () => (
-  <div className={styles.Inventory} data-testid="Inventory">
-    Inventory Component
-  </div>
+  <Layout>
+    <div className={styles.Inventory} data-testid="Inventory">
+      Inventory Component
+    </div>
+  </Layout>
 );
 
 export default Inventory;
