6ed8789b76250d6845793c1bc3dcf3675f625908
Liam Gulliver <128083+lgulliver@users.noreply.github.com>
2021-11-24 17:54:15 +0000

    start of new inventoryitem

---
+++ src/CarsUnlimited.InventoryAPI/Entities/InventoryImage.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarsUnlimited.InventoryShared.Entities {
+    public class InventoryImage {
+        public string ImageUrl { get; set; }
+        public bool IsPrimary { get; set; }
+    }
+}
---
+++ src/CarsUnlimited.InventoryAPI/Entities/InventoryItem.cs
@@ -0,0 +1,21 @@
+﻿using CarsUnlimited.Shared.Attributes;
+using CarsUnlimited.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarsUnlimited.InventoryShared.Entities
+{
+    [BsonCollection("inventory")]
+    public class InventoryItem : Document
+    {
+        public string Category { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string Sku { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int InStock { get; set; }
+        public List<InventoryImage> Images { get; set; }        
+    }
+}
