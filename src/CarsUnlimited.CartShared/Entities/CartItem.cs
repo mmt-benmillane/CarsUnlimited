8b8d5c92772632f465279191c85331baa58374a8
Liam Gulliver <128083+lgulliver@users.noreply.github.com>
2021-11-29 14:56:35 +0000

    updated cart to use new inventory item

--- src/CarsUnlimited.CartShared/Entities/CartItem.cs
+++ src/CarsUnlimited.CartShared/Entities/CartItem.cs
@@ -8,7 +8,7 @@ namespace CarsUnlimited.CartShared.Entities
     public class CartItem
     {
         public string SessionId { get; set; }
-        public string CarId { get; set; }
+        public string ItemId { get; set; }
         public int Count { get; set; }
     }
 }
