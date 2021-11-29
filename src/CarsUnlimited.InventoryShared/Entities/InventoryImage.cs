14aab60bff50e7411608f266891fe0c4b82746a5
Liam Gulliver <128083+lgulliver@users.noreply.github.com>
2021-11-29 15:08:06 +0000

    added items to inventoryshared

---
+++ src/CarsUnlimited.InventoryShared/Entities/InventoryImage.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarsUnlimited.InventoryShared.Entities 
+{
+    public class InventoryImage {
+        public string ImageUrl { get; set; }
+        public bool IsPrimary { get; set; }
+    }
+}
